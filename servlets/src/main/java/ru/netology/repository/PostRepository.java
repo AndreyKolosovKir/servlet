bc06c0c035c4a596f31a72e5e39768d6ea9b8b06
AndreyKolosovKir <andrey.e.04@gmail.com>
2023-08-24 22:29:23 +0300

    New commit. Add Annotatin Config

--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,10 +1,11 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.*;
-
+@Repository
 public class PostRepository {
 
     private Map<Long, Post> posts;
